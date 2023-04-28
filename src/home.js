b7b9f7ec3254f5703f44482a274d828d40370a7c
Dharmi Patel <122154176+dhaar71@users.noreply.github.com>
2023-04-28 17:10:01 +0530

    home.js

--- src/home.js
+++ src/home.js
@@ -1,18 +1,17 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( 
         <div className="Box1">
             <h1 className="main-title text-center"> Login/Register </h1>
             <div className="buttons">
-            <Link to="/login">
+            <a href="login.js" target="_blank" rel="no referrer"><button className="primary-button"> LOG IN </button></a>
+            {/* <Link to="www.google.com" target="_blank" >
                 <button className="primary-button"> LOG IN </button></Link>
-            <Link to="/Register">
+            <Link to="/Register" target="_blank">
                 <button className="primary-button"> Register </button></Link>
-            
+             */}
         </div>
         </div>
      );
 }
- 
-export default Home;
